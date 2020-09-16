7b240c0df47a7a4e7b2875dd5e49e979ff0e1d54
sorozco <orozcohoyos123@gmail.com>
2020-09-15 19:33:48 -0500

    componentes

---
+++ 5-vue-cli/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>server status: {{ status }}</p>
+    <hr />
+    <button @click="changeStatus">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      status: "critical",
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = "normal";
+    },
+  },
+};
+</script>
