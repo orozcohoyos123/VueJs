3ddfe795fe579a63ac6bcc126c0b27ef87a79ce2
Sebastian Orozco <sebastian.orozco@sinco.com.co>
2020-09-28 17:06:12 -0500

    second project

---
+++ 9-second-project/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <app-quote-grid></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue'
+
+    export default {
+        data() {
+            return {
+                quotes: ['Some quote to see something'],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>
