cfade62d38b05f28cf2f5850d5cde73099cc0e7c
Sebastian Orozco <sebastian.orozco@sinco.com.co>
2020-10-09 16:54:19 -0500

    directives

--- 10-forms/src/App.vue
+++ 10-forms/src/App.vue
@@ -98,17 +98,23 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -127,7 +133,7 @@
                         </ul>
                         <p>Gender: {{gender}}</p>
                         <p>Priority: {{selectedPriority}}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{dataSwitch}}</p>
                     </div>
                 </div>
             </div>
@@ -136,6 +142,8 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue'
+
     export default {
         data() {
             return {
@@ -148,7 +156,17 @@
                 sendMail: [],
                 gender: 'Male',
                 selectedPriority: 'High',
-                priorities: ['High', 'Medium', 'Low']
+                priorities: ['High', 'Medium', 'Low'],
+                dataSwitch: true,
+                isSubmitted: false
+            }
+        },
+        components: {
+            appSwitch: Switch
+        },
+        methods: {
+            submitted(){
+                this.isSubmitted = true;
             }
         },
     }
---
+++ 11-directives/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight',{
+  bind(el, binding, vnode){
+    el.style.backgroundColor = 'green';
+  }
+});
+
+Vue.directive('highlightWithParameter',{
+  bind(el, binding, vnode){
+    el.style.backgroundColor = binding.value;
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
