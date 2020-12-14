be534b51fd187ecbddf258a19b57f1268acec92a
Sebastian Orozco <sebastian.orozco@sinco.com.co>
2020-12-14 08:31:45 -0500

    filters

---
+++ 12-filters-mixins/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <p>{{text | toUppercase}}</p>
+                <p>{{globalText | toLowercase}}</p>
+                <p>{{textMultipleFilters | toUppercase | toLowercase}}</p>
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits" >{{fruit}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: 'local filter',
+                globalText: 'GLOBAL FILTER',
+                textMultipleFilters: 'Multiple filter text',
+                fruits: ["Banana", "Melon", "Apple", "Strawberry"],
+                filterText: ""
+            }
+        },
+        filters: {
+            toUppercase(value){
+                return value.toUpperCase();
+            }
+        },
+        computed:{
+            filteredFruits(){
+                return this.fruits.filter((elm) => {
+                    return elm.match(this.filterText);
+                });
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
